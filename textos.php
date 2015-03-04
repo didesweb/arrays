e5ff54b499ced7be6fe5588802d5e8281f9e82c0
Roque Arnas <roque.arnas@didesweb.com>
2015-03-04 20:03:33 +0100

    Create textos.php

---
+++ textos.php
@@ -0,0 +1,18 @@
+<!-- Didesweb -->  
+<!-- Diseño y desarrollo web -->  
+<!-- http://didesweb.com/-->  
+<!-- Este obra está protegida bajo licencia Creative Commons Attribution --> 
+<?php
+	$titulos=array(
+		'enc_1'=>"Titulo Grande",
+		'enc_2'=>"Titulo pequeño",
+	);
+	$enlaces=array(
+		'link_1'=>"Enlace 1",
+		'link_2'=>"Enlace 2",
+	);
+	$estilos=array(
+		'enlaces_normales'=>"enlaces",
+		'enlaces_destacados'=>"enlaces_dest",
+	);
+?>
